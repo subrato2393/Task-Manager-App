aff92d300fb259da3b8d953cb24f61747d347f04
subrato2393 <subrato2393@gmail.com>
2022-12-10 13:19:16 +0600

    integrate bootstrap and routing

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path:  '',
+    redirectTo:'dashboard',
+    pathMatch: 'full', 
+  },
+  { 
+    path:  'dashboard',
+    component:  DashboardComponent
+  },
+  { 
+    path:  'contact', 
+    component:  ContactComponent
+  },
+  { 
+    path:  'about', 
+    component:  AboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
