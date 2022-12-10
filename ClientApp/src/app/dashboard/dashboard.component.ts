e79a5e5834246a6c697c44a5bd943959c651c2c7
subrato2393 <subrato2393@gmail.com>
2022-12-10 23:53:18 +0600

    load dynamic data from component instead of using static value in html

--- ClientApp/src/app/dashboard/dashboard.component.ts
+++ ClientApp/src/app/dashboard/dashboard.component.ts
@@ -18,10 +18,12 @@ export class DashboardComponent implements OnInit {
   AvailableFunds: any;
 
   Clients: any;
-  Projects: any;
+  Projects:any;
   Years: number[] = [];
-  TeamMembersSummary = [];
-  TeamMembers = [];
+  ProjectBreafing:any[]=[];
+  TeamMembersSummary:any[] = [];
+
+  TeamMembers:any[] = [];
 
   constructor() { 
  
@@ -39,5 +41,71 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+
+    this.Projects=[
+      {Name : "Project A"},
+      {Name : "Project B"},
+      {Name : "Project C"},
+      {Name : "Project D"},
+  ];
+
+  for (let i=2019;i>2000;i--){
+    this.Years.push(i);
+  };
+
+  this.ProjectBreafing=[
+    {Title:"Project Cost", Cost:"$2113507"},
+    {Title:"Current Expenditure", Cost:"$96788"},
+    {Title:"Available Funds", Cost:"$52536"}
+  ];
+
+  this.TeamMembersSummary=[
+    {Region:"East", TeamMemberCount:20, TemporaryUnAvailableMember:4},
+    {Region:"South", TeamMemberCount:15, TemporaryUnAvailableMember:8},
+    {Region:"West", TeamMemberCount:17, TemporaryUnAvailableMember:1},
+    {Region:"North", TeamMemberCount:15, TemporaryUnAvailableMember:6}
+  ];
+
+  this.Clients=["ABC Infotech Ltd.","DEF Software Solutions","GHI Industries","ITIL"];
+
+this.TeamMembers =[
+  {
+    Region:"East",Members:
+    [
+        {Id:"1",Name:"Ford",Status:"Available"},
+        {Id:"2",Name:"Miller",Status:"Not Available"},
+        {Id:"3",Name:"Jones",Status:"Busy"},
+        {Id:"4",Name:"James",Status:"Busy"}
+    ],
+  },
+  {
+    Region:"South",Members:
+    [
+        {Id:"1",Name:"Jon",Status:"Available"},
+        {Id:"2",Name:"Abraham",Status:"Available"},
+        {Id:"3",Name:"Jones",Status:"Busy"},
+        {Id:"4",Name:"Deva",Status:"Busy"}
+    ],
+  },
+  {
+    Region:"West",Members:
+    [
+        {Id:"1",Name:"Pen",Status:"Available"},
+        {Id:"2",Name:"Miller",Status:"Available"},
+        {Id:"3",Name:"Jones",Status:"Busy"},
+        {Id:"4",Name:"O Neill",Status:"Busy"}
+    ],
+  },
+  {
+    Region:"North",Members:
+    [
+        {Id:"1",Name:"Ford",Status:"Available"},
+        {Id:"2",Name:"Super",Status:"Available"},
+        {Id:"3",Name:"Really",Status:"Busy"},
+        {Id:"4",Name:"James",Status:"Busy"}
+    ]
   }
+]
+  };
+
 }
